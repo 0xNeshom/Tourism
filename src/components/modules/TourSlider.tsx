4b3e64e298d162c75ec4b65f5859309342e3306d
0xNeshom <MohsenGhanbari2003@gmail.com>
2024-11-08 17:17:44 +0330

    bug-fix

--- src/components/modules/TourSlider.tsx
+++ src/components/modules/TourSlider.tsx
@@ -1,21 +1,51 @@
+'use client'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { Navigation, Autoplay } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import img from '@/public/isfahan1.svg'
 const TourSlider = () => {
   return (
-    <div>    <Swiper
-    spaceBetween={50}
-    slidesPerView={3}
-    onSlideChange={() => console.log('slide change')}
-    onSwiper={(swiper) => console.log(swiper)}
-  >
-    <SwiperSlide>Slide 1</SwiperSlide>
-    <SwiperSlide>Slide 2</SwiperSlide>
-    <SwiperSlide>Slide 3</SwiperSlide>
-    <SwiperSlide>Slide 4</SwiperSlide>
-  </Swiper>
-  </div>
+    <div>
+      <Swiper
+         slidesPerView={3}
+         spaceBetween={30}
+         dir="rtl"
+         autoplay={{ delay: 1500, disableOnInteraction: false }}
+         //   rewind={true}
+         loop={true}
+         navigation={true}
+         modules={[Navigation, Autoplay]}
+         className="mySwiper articles_slider"
+      >
+        <SwiperSlide>
+          <img src='https://limoo.host/asstes/img/hosts/down02.png' alt='تصویر 1' />
+          <h3>عنوان اسلاید</h3>
+          <p>توضیحات اسلاید</p>
+        </SwiperSlide><SwiperSlide>
+          <img src='https://limoo.host/asstes/img/hosts/down02.png' alt='تصویر 1' />
+          <h3>عنوان اسلاید</h3>
+          <p>توضیحات اسلاید</p>
+        </SwiperSlide><SwiperSlide>
+          <img src='https://limoo.host/asstes/img/hosts/down02.png' alt='تصویر 1' />
+          <h3>عنوان اسلاید</h3>
+          <p>توضیحات اسلاید</p>
+        </SwiperSlide><SwiperSlide>
+          <img src='https://limoo.host/asstes/img/hosts/down02.png' alt='تصویر 1' />
+          <h3>عنوان اسلاید</h3>
+          <p>توضیحات اسلاید</p>
+        </SwiperSlide>
 
-  )
-}
+        <SwiperSlide><img src='https://limoo.host/asstes/img/hosts/down02.png' alt='تصویر 1' />
+          <h3>عنوان اسلاید</h3>
+          <p>توضیحات اسلاید</p></SwiperSlide>
+        <SwiperSlide>اسلاید 3<img src='https://limoo.host/asstes/img/hosts/down02.png' alt='تصویر 1' />
+          <h3>عنوان اسلاید</h3>
+          <p>توضیحات اسلاید</p></SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
 
-export default TourSlider
+export default TourSlider;
