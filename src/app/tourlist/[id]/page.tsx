9ddbe9c7dacd5269658b62d3e794823301d761bd
0xNeshom <MohsenGhanbari2003@gmail.com>
2024-11-12 14:53:56 +0330

    mock test removed

--- src/app/tourlist/[id]/page.tsx
+++ src/app/tourlist/[id]/page.tsx
@@ -1,3 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+
 // import Image from 'next/image'
 // import { notFound } from 'next/navigation'
 
