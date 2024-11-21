d6244f146ea8e07d038de315fdf9a6bd955cdbd9
0xNeshom <MohsenGhanbari2003@gmail.com>
2024-11-21 18:02:38 +0330

    folder auth made

--- src/app/(auth)/login-register/page.jsx
+++ src/app/(auth)/login-register/page.jsx
@@ -1,20 +1,20 @@
-'use client'
+'use client';
 import React from 'react';
 
-import {useState} from "react";
-import {authTypes} from '@/utilities/constants.js';
-import  Login  from '@/components/templates/login-register/Login.js';
+import { useState } from 'react';
+import { authTypes } from '@/utilities/constants.js';
+import Login from '@/components/templates/login-register/Login.js';
 import Register from '@/components/templates/login-register/Register';
 const Page = () => {
-  const [authType,setAuthType] = useState(authTypes.LOGIN)
+  const [authType, setAuthType] = useState(authTypes.LOGIN);
 
   const showRegisterForm = () => setAuthType(authTypes.REGISTER);
   const showloginForm = () => setAuthType(authTypes.LOGIN);
-  console.log("Login Button Clicked"); 
-  console.log("Register Button Clicked"); 
+  console.log('Login Button Clicked');
+  console.log('Register Button Clicked');
   return (
     <div>
-      <div  >
+      <div>
         {authType === authTypes.LOGIN ? (
           <Login showRegisterForm={showRegisterForm} />
         ) : (
