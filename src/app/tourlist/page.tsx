43eac4af8f7c68dde9be48ffa6c924c259c56085
0xNeshom <MohsenGhanbari2003@gmail.com>
2024-11-26 10:54:01 +0330

    tourlist bugs

--- src/app/tourlist/page.tsx
+++ src/app/tourlist/page.tsx
@@ -1,11 +1,14 @@
 // /pages/tour-list.tsx
 import React from 'react';
 import TourCard from '@/components/templates/TourCard';
-
+import Footer from '@/components/templates/footer/Footer';
+import Navbar from '@/components/modules/Navbar';
 const TourList: React.FC = () => {
   return (
     <div>
+      <Navbar/>
       <TourCard params={{ id: '' }} />
+      <Footer/>
     </div>
   );
 };
