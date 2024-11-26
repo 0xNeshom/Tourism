c5417ba618c1962dc89350f484bfedd02193ee72
0xNeshom <MohsenGhanbari2003@gmail.com>
2024-11-26 10:08:22 +0330

    build

--- src/components/modules/Category.tsx
+++ src/components/modules/Category.tsx
@@ -10,32 +10,31 @@ import luggage from '@/public/category/Frame 1-6.svg';
 
 const categories = [
   { id: 1, href: './tourlist', img: forest, label: 'جنگلی' },
-  { id: 2, href: './tourlist', img: umbrella, label: 'ساحلی'},
-  { id: 3, href: './tourlist', img: place, label: 'زیارتی'},
-  { id: 4, href: './tourlist', img: group, label: 'بومگردی'},
-  { id: 5, href: './tourlist', img: map, label: 'تاریخی'},
-  { id: 6, href: './tourlist', img: airicon, label: 'خارجی'},
-  { id: 7, href: './tourlist', img: luggage, label: 'همه تورها'},
+  { id: 2, href: './tourlist', img: umbrella, label: 'ساحلی' },
+  { id: 3, href: './tourlist', img: place, label: 'زیارتی' },
+  { id: 4, href: './tourlist', img: group, label: 'بومگردی' },
+  { id: 5, href: './tourlist', img: map, label: 'تاریخی' },
+  { id: 6, href: './tourlist', img: airicon, label: 'خارجی' },
+  { id: 7, href: './tourlist', img: luggage, label: 'همه تورها' },
 ];
 
 const Category: React.FC = () => {
   return (
     <div className='px-2 justify-center flex'>
-      <div className='grid grid-cols-4 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-7 gap-1 xs:gap-3 md:gap-10 justify-items-center justify-center md:mx-10 mx-auto '>
+      <div className='md:flex  md:items-center  md:justify-center gap-4 mx-auto md:overflow-auto grid  grid-cols-4 justify-center items-center max-sm:grid-cols-2 max-[413px]:grid-cols-1  '>
         {categories.map((category) => (
-          <div key={category.id} className="w-full ">
-            <Link 
-              href={category.href} 
-              className='flex flex-col justify-center items-center border border-primary rounded-xl xs:rounded-3xl
+          <div key={category.id} className='w-full min-w-[160px] mx-auto '>
+            <Link
+              href={category.href}
+              className='flex flex-col justify-center items-center border border-primary rounded-xl mx-auto xs:rounded-3xl
                 w-20 h-20 xs:w-20 xs:h-20 md:w-40 md:h-44 
                 gap-0.5 xs:gap-1 md:gap-2 md:pb-2'
             >
-              <Image 
-                src={category.img} 
-                alt={category.label} 
-            
+              <Image
+                src={category.img}
+                alt={category.label}
                 priority={true}
-                className='w-8 xs:w-8 md:w-auto h-auto ' 
+                className='min-w-8  md:w-auto h-auto '
               />
               <span className='text-xs xs:text-xs md:text-xl text-center leading-tight'>
                 {category.label}
