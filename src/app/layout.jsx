bbff7646708f1f13c9fdf704641f91f1e6cb771b
0xNeshom <MohsenGhanbari2003@gmail.com>
2024-11-26 11:57:59 +0330

    add vercel speed insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,8 @@ import ScrollTop from '@/utilities/scrollToTop/ScrollToTop';
 import './globals.css';
 import localFont from 'next/font/local';
 import Provider from '@/utilities/reactQueryProvider/ReactQuery';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 const iranSans = localFont({
   src: [
     { path: '../public/fonts/IRANSansX-Regular.woff2' },
@@ -19,11 +21,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang='fa' dir='rtl'>
       <Head>
-      <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-Variable-font-face.css" rel="stylesheet" type="text/css" />
+        <link
+          href='https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-Variable-font-face.css'
+          rel='stylesheet'
+          type='text/css'
+        />
       </Head>
-      <body className={` antialiased max-w-[1550px] mx-auto`} >
+      <body className={` antialiased max-w-[1550px] mx-auto`}>
         <Provider>{children}</Provider>
         <ScrollTop />
+        <SpeedInsights
+          url='https://tourism-388dntoly-mohsens-projects-b89f5647.vercel.app/' 
+          view='desktop'
+          threshold={50}
+        />
       </body>
     </html>
   );
